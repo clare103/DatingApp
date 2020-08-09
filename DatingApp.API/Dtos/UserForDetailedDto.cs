3fee1797a67cf1c31816486a11d9691ba9ef1611
Ross Clare <clare103@mail.chapman.edu>
2020-08-08 18:50:22 -0700

    end of s8

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -1,21 +1,18 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.models;
 
-namespace DatingApp.API.models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id {get; set; }
 
         public string Username {get; set;}
 
-        public byte[] PasswordHash {get; set;}
-
-        public byte[] PasswordSalt {get; set;}
-
         public string Gender {get; set;}
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs {get; set; }
 
@@ -33,7 +30,9 @@ namespace DatingApp.API.models
         
         public string Country {get; set;}
 
-        public ICollection<Photo> Photos {get; set;}
+        public string PhotoURL {get; set;}
+
+        public ICollection<PhotosForDetailedDto> Photos {get; set;}
 
     }
 }
